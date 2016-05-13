5b8742ad7fd0788a3b641e83a5f48bc3badbab25
Joe <joew@fnvi.co.uk>
2016-05-13 14:14:25 +0100

    Added settings

--- settings.php
+++ settings.php
@@ -1,8 +1,4 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+define("MONGOURI","mongodb://FNVJoe:p455w0rd@localhost");
+define("DATABASE","messaging");
